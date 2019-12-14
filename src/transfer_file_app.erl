a049cc4bf5b5b1a7bb7a1d8b4355d24e6a40b752
Innaky <innaky@protonmail.com>
2019-12-13 21:00:55 -0400

    First commit, this project execute a process in a remote erlang node The main function is loop (an apply). The usable functions are get_file for "copy" a file, and list_files for list the files. This functions are a wrapper for the file module.

---
+++ src/transfer_file_app.erl
@@ -0,0 +1,37 @@
+%%%-------------------------------------------------------------------
+%% @doc transfer_file public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(transfer_file_app).
+
+-behaviour(application).
+
+-export([start/1, rpc/4, get_file/2, list_files/1).
+
+start(Node) ->
+    spawn(fun() ->
+		  loop() end).
+
+rpc(Pid, Module, Fun, Args) ->
+    Pid ! {rpc, self(), Module, Fun, Args},
+    receive
+	{Pid, Response} ->
+	    Response
+    end.
+
+get_file(Pid, File) ->
+    {ok, Binary} = transfer_file_app:rpc(Pid, file, read_file, [File]),
+    file:write_file(File, [Binary]).
+
+list_files(Pid) ->
+    transfer_file_app:rpc(Pid, file, list_dir, ["."]).
+
+%% internal functions
+
+loop() ->
+    receive
+	{rpc, Pid, Module, Fun, Args} ->
+	    Pid ! {self(), (catch apply(Module, Fun, Args))},
+	    loop()
+    end.
